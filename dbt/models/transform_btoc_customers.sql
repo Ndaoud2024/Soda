c05f9c9c1545ea2007ff464223da4b7a7b1bf5d1
Ndaoud2024 <ndaoud@jems-group.com>
2025-03-18 16:44:48 +0100

    Update transform_btoc_customers.sql

--- dbt/models/transform_btoc_customers.sql
+++ dbt/models/transform_btoc_customers.sql
@@ -1,24 +1,21 @@
-create  table "postgres"."public"."transform_btoc_customers__dbt_tmp_workflow"
-    as
-  (
-WITH cleaned_data AS (
-    SELECT
-        id,
-        created_at,
-        LOWER("Gender") AS gender_normalized, 
-        "Title",
-        "FirstName",
-        "Surname",
-        "StreetAddress",
-        "Country",
-        "CountryFull",
-        "EmailAddress",
-        chiffreaffaire,
-        bandecommande
-    FROM "postgres"."public"."btoc_customers"  
-    WHERE "EmailAddress" IS NOT NULL  
-)
-SELECT *
-FROM cleaned_data
-  );
-  
+create table postgres.public.transform_btoc_customers as (
+    WITH cleaned_data AS (
+        SELECT
+            id,
+            created_at,
+            LOWER("Gender") AS gender_normalized, 
+            "Title",
+            "FirstName",
+            "Surname",
+            "StreetAddress",
+            "Country",
+            "CountryFull",
+            "EmailAddress",
+            chiffreaffaire,
+            bandecommande
+        FROM postgres.public.btoc_customers  
+        WHERE "EmailAddress" IS NOT NULL  
+    )
+    SELECT *
+    FROM cleaned_data
+);
