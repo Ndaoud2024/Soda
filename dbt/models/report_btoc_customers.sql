76ae0372f3d2d6853be937557f9b6c3fcd82b322
Ndaoud2024 <ndaoud@jems-group.com>
2025-03-18 14:54:28 +0100

    Create report_btoc_customers.sql

---
+++ dbt/models/report_btoc_customers.sql
@@ -0,0 +1,15 @@
+create view "postgres"."public"."report_btoc_customers__dbt_tmp"
+    
+    
+  as (
+    
+
+SELECT
+    "Country",
+    COUNT(*) AS total_customers,
+    AVG(chiffreaffaire) AS avg_revenue,
+    SUM(chiffreaffaire) AS total_revenue,
+    AVG(bandecommande) AS avg_order_band
+FROM "postgres"."public"."transform_btoc_customers"
+GROUP BY "Country"
+  );
